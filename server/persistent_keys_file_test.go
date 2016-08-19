643d7d6b36474ea04a158ce772dde444647fc211
Ricardo Murillo <ricardo.murillo@adroll.com>
2016-08-19 15:19:27 -0600

    Updated server test case

--- server/persistent_keys_file_test.go
+++ server/persistent_keys_file_test.go
@@ -1,6 +1,7 @@
 package server_test
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/AdRoll/hologram/server"
@@ -10,7 +11,8 @@ import (
 func TestPersistentKeysFile(t *testing.T) {
 	data := `{
         "KEY1": {"username": "user1", "password": "pass1", "roles": ["role1", "role11"]},
-        "KEY2": {"username": "user2", "password": "pass2", "roles": ["role2", "role22"]}
+        "KEY2": {"username": "user2", "password": "pass2", "roles": ["role2", "role22"]},
+        "KEY3": {"username": "user1", "password": "pass1", "roles": ["role111", "role1111"]}
     }`
 
 	open := func() ([]byte, error) {
@@ -31,12 +33,16 @@ func TestPersistentKeysFile(t *testing.T) {
 		Convey("An existing key in file should be found", func() {
 			pkf := server.NewPersistentKeysFile(open, dump, "username", "roles")
 
+			keys := []string{"KEY3", "KEY1"}
+			sort.Strings(keys)
 			expected := map[string]interface{}{
 				"username":      "user1",
-				"sshPublicKeys": []string{"KEY1"},
+				"sshPublicKeys": keys,
 				"password":      "pass1",
 			}
 			actual, err := pkf.Search("user1")
+			actualKeys := actual["sshPublicKeys"]
+			sort.Strings(actualKeys.([]string))
 			So(err, ShouldBeNil)
 			So(actual, ShouldResemble, expected)
 		})
