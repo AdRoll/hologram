2e02156a3e99f940db1782abd1d8d7aeb651784c
Ricardo Murillo <ricardo.murillo@adroll.com>
2016-08-22 20:07:51 -0600

    Update test cases

--- server/server_test.go
+++ server/server_test.go
@@ -151,7 +151,7 @@ func TestServerStateMachine(t *testing.T) {
 			sshKeys:  []string{},
 			req:      neededModifyRequest,
 		}
-		testServer := server.New(authenticator, &dummyCredentials{}, "default", g2s.Noop(), ldap, "cn", "dc=testdn,dc=com", false, "")
+		testServer := server.New(authenticator, &dummyCredentials{}, "default", g2s.Noop(), ldap, false)
 		r, w := io.Pipe()
 
 		testConnection := protocol.NewMessageConnection(ReadWriter(r, w))
