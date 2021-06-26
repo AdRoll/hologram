40a4bbe9b863635215906aea85cfa8862423ef4c
Marco Ippolito <marco.ippolito@nextroll.com>
2021-06-26 19:19:12 -0300

    Invert if clause

--- agent/support/linux/init.sh
+++ agent/support/linux/init.sh
@@ -21,11 +21,11 @@ start)
   ip addr add 169.254.169.254/24 broadcast 169.254.169.255 dev lo:metadata
   ip link set dev lo:metadata up
   pid=$("$DAEMON" $DAEMONOPTS &> /var/log/hologram.log & echo $!)
-  if [ -z $pid ]; then
-      printf '%s\n' Fail
-  else
+  if [[ $pid ]]; then
       echo $pid > $PIDFILE
       printf '%s\n' Ok
+  else
+      printf '%s\n' Fail
   fi
 ;;
 status)
