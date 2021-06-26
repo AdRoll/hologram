6948891855d1b7c34be51b83f58fe5e524723314
Marco Ippolito <marco.ippolito@nextroll.com>
2021-06-26 19:13:51 -0300

    Remove unneeded quoting

--- agent/support/linux/init.sh
+++ agent/support/linux/init.sh
@@ -4,17 +4,17 @@
 # description: Hologram agent.
 # processname: hologram-agent
 
-DAEMON_PATH="/usr/local/bin"
+DAEMON_PATH=/usr/local/bin
 
-DAEMON="/usr/local/bin/hologram-agent"
-DAEMONOPTS=""
+DAEMON=/usr/local/bin/hologram-agent
+DAEMONOPTS=
 
-NAME="hologram-agent"
+NAME=hologram-agent
 DESC="AWS Credentials Agent"
 PIDFILE=/var/run/$NAME.pid
 SCRIPTNAME=/etc/init.d/$NAME
 
-case "$1" in
+case $1 in
 start)
   printf "%-50s" "Starting $NAME..."
   cd $DAEMON_PATH
@@ -24,10 +24,10 @@ start)
   PID=`$DAEMON $DAEMONOPTS > /var/log/hologram.log 2>&1 & echo $!`
   #echo "Saving PID" $PID " to " $PIDFILE
         if [ -z $PID ]; then
-            printf "%s\n" "Fail"
+            printf "%s\n" Fail
         else
             echo $PID > $PIDFILE
-            printf "%s\n" "Ok"
+            printf "%s\n" Ok
         fi
 ;;
 status)
@@ -37,7 +37,7 @@ status)
             if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
                 printf "%s\n" "Process dead but pidfile exists"
             else
-                echo "Running"
+                echo Running
             fi
         else
             printf "%s\n" "Service not running"
@@ -49,7 +49,7 @@ stop)
             cd $DAEMON_PATH
         if [ -f $PIDFILE ]; then
             kill -TERM $PID
-            printf "%s\n" "Ok"
+            printf "%s\n" Ok
             rm -f $PIDFILE
         else
             printf "%s\n" "pidfile not found"
