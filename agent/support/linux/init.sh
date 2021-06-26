18f153cdd1513ab8a017f0948b0e02193b2ccf67
Marco Ippolito <marco.ippolito@nextroll.com>
2021-06-26 19:14:04 -0300

    Single-quote for non-expansions

--- agent/support/linux/init.sh
+++ agent/support/linux/init.sh
@@ -9,13 +9,13 @@ NAME=hologram-agent
 DAEMON=$DAEMON_PATH/$NAME
 DAEMONOPTS=
 
-DESC="AWS Credentials Agent"
+DESC='AWS Credentials Agent'
 PIDFILE=/var/run/$NAME.pid
 SCRIPTNAME=/etc/init.d/$NAME
 
 case $1 in
 start)
-  printf "%-50s" "Starting $NAME..."
+  printf '%-50s' "Starting $NAME..."
   cd $DAEMON_PATH
   # Make sure that the metadata interface is up.
   ip addr add 169.254.169.254/24 broadcast 169.254.169.255 dev lo:metadata
@@ -23,35 +23,35 @@ start)
   PID=`$DAEMON $DAEMONOPTS > /var/log/hologram.log 2>&1 & echo $!`
   #echo "Saving PID" $PID " to " $PIDFILE
         if [ -z $PID ]; then
-            printf "%s\n" Fail
+            printf '%s\n' Fail
         else
             echo $PID > $PIDFILE
-            printf "%s\n" Ok
+            printf '%s\n' Ok
         fi
 ;;
 status)
-        printf "%-50s" "Checking $NAME..."
+        printf '%-50s' "Checking $NAME..."
         if [ -f $PIDFILE ]; then
             PID=`cat $PIDFILE`
             if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
-                printf "%s\n" "Process dead but pidfile exists"
+                printf '%s\n' "Process dead but pidfile exists"
             else
                 echo Running
             fi
         else
-            printf "%s\n" "Service not running"
+            printf '%s\n' 'Service not running'
         fi
 ;;
 stop)
-        printf "%-50s" "Stopping $NAME"
+        printf '%-50s' "Stopping $NAME"
             PID=`cat $PIDFILE`
             cd $DAEMON_PATH
         if [ -f $PIDFILE ]; then
             kill -TERM $PID
-            printf "%s\n" Ok
+            printf '%s\n' Ok
             rm -f $PIDFILE
         else
-            printf "%s\n" "pidfile not found"
+            printf '%s\n' 'pidfile not found'
         fi
 ;;
 
