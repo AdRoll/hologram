5214cf0a2e40327201126ed5b24a435af931926e
Marco Ippolito <marco.ippolito@nextroll.com>
2021-06-27 07:00:43 -0300

    Fix typo in docs

--- buildscripts/returncodes.sh
+++ buildscripts/returncodes.sh
@@ -1,5 +1,5 @@
 # List of all return codes that we can return with an explanation.
-# This way a client could figure out what happeend just by looking at our return code.
+# This way a client could figure out what happened just by looking at our return code.
 
 export ERRTEST=1    # Error found when running tests
 export ERRCOMPILE=2 # Error found compiling hologram
