5f5d68dc086a832a27d75708dfa2bac163418818
Josh Mullins <mullinsjoshuaj@gmail.com>
2022-01-06 11:43:34 -0500

    Added version command

--- buildscripts/compile_hologram.sh
+++ buildscripts/compile_hologram.sh
@@ -4,11 +4,13 @@ source ${HOLOGRAM_DIR}/buildscripts/returncodes.sh
 
 # go get -u github.com/kardianos/govendor
 
+export GIT_TAG=$(git describe --tags --long)
+
 echo "Compiling for linux..."
-GOOS=linux  go install github.com/AdRoll/hologram/... || exit ${ERRCOMPILE}
+GOOS=linux  go install -ldflags="-X 'main.Version=${GIT_TAG}'" github.com/AdRoll/hologram/... || exit ${ERRCOMPILE}
 
 echo "Compiling for osx"
-GOOS=darwin go install github.com/AdRoll/hologram/... || exit ${ERRCOMPILE}
+GOOS=darwin go install -ldflags="-X 'main.Version=${GIT_TAG}'" github.com/AdRoll/hologram/... || exit ${ERRCOMPILE}
 
 echo "Running tests..."
 go test -v github.com/AdRoll/hologram/... || exit ${ERRTEST}
